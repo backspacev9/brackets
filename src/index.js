0e373290ed9610a3fc518300fee3bc26aba53ed3
backspacev9 <backspacev9@gmail.com>
2021-03-13 18:45:58 +0300

    refactor: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var reworked = []; 
+  for (let i = 0; i < bracketsConfig.length; i++){
+    reworked[i] = bracketsConfig[i].join('');
+  }
+  console.log('reworked: '+reworked ); 
+  var length =str.length;
+  var prevStr = '';
+  for (let i = 0; i < length; i++){
+    prevStr = str;
+    for (let k = 0; k < reworked.length; k++){  
+      str = str.replace(reworked[k],'');
+    }
+    if (str == prevStr || str == ''){
+      break;
+    }  
+  }
+  if (str.length > 0){
+    return false
+  } else return true
 }
+
+
